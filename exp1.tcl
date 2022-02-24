e999feae1a08e4500fac935e079847a9992f790f
Brian Nguyen <59807727+bachnguyen91097@users.noreply.github.com>
2022-02-24 09:35:06 -0500

    Testing set up for exp1

---
+++ exp1.tcl
@@ -0,0 +1,88 @@
+# Create a Simulator object
+set ns [new Simulator]
+
+# TCP variant and  CBR rate
+set variant [lindex $argv 0]
+set rate [lindex $argv 1]
+
+# Open the trace file
+set tf [open ${variant}_output_${rate}.tr w]
+$ns trace-all $tf
+
+# Define a 'finish' procedure
+proc finish {} {
+	global ns tf
+	$ns flush-trace
+	close $tf
+	exit 0
+}
+
+# Create 6 nodes
+set n1 [$ns node]
+set n2 [$ns node]
+set n3 [$ns node]
+set n4 [$ns node]
+set n5 [$ns node]
+set n6 [$ns node]
+
+
+# Create links between the nodes
+$ns duplex-link $n1 $n2 10Mb 10ms DropTail
+$ns duplex-link $n5 $n2 10Mb 10ms DropTail
+$ns duplex-link $n3 $n2 10Mb 10ms DropTail
+$ns duplex-link $n3 $n4 10Mb 10ms DropTail
+$ns duplex-link $n3 $n6 10Mb 10ms DropTail
+
+
+# Setup a TCP connection
+set tcp [new Agent/variant]
+$tcp set class_ 2
+$ns attach-agent $n1 $tcp
+set sink [new Agent/TCPSink]
+$ns attach-agent $n4 $sink
+$ns connect $tcp $sink
+$tcp set fid_ 1
+
+# Setup FTP over TCP
+set ftp [new Application/FTP]
+$ftp attach-agent $tcp
+$ftp set type_ FTP
+
+
+
+# Add a CBR source at N2
+# Setup a UDP connection
+set udp [new Agent/UDP]
+$ns attach-agent $n2 $udp
+set null [new Agent/Null]
+$ns attach-agent $n3 $null
+$ns connect $udp $null
+$udp set fid_ 2
+
+# Setup a CBR over UDP connection
+set cbr [new Application/Traffic/CBR]
+$cbr attach-agent $udp
+$cbr set type_ CBR
+$cbr set packet_size_ 1000
+$cbr set rate_ ${rate}mb
+$cbr set random_ false
+
+
+#Schedule events for the CBR and FTP agents
+$ns at 0.1 "$cbr start"
+$ns at 1.0 "$ftp start"
+$ns at 8.5 "$ftp stop"
+$ns at 9.5 "$cbr stop"
+
+#Call the finish procedure after  seconds of simulation time
+$ns at 10.0 "finish"
+
+#Print CBR packet size and interval
+puts "CBR packet size = [$cbr set packet_size_]"
+puts "CBR interval = [$cbr set interval_]"
+
+#Run the simulation
+$ns run
+
+close $tf
+
