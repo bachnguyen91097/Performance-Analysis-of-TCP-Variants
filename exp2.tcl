06f5d99e0531d0b9463a9688d1c17324656c71a6
Brian Nguyen <59807727+bachnguyen91097@users.noreply.github.com>
2022-02-25 22:46:33 -0500

    Modify a bit in exp1 and add exp2

--- exp2.tcl
+++ exp2.tcl
@@ -1,12 +1,13 @@
 # Create a Simulator object
 set ns [new Simulator]
 
-# TCP variant and  CBR rate
-set variant [lindex $argv 0]
-set rate [lindex $argv 1]
+# TCP variants and  CBR rate
+set v1 [lindex $argv 0]
+set v2 [lindex $argv 1]
+set rate [lindex $argv 2]
 
 # Open the trace file
-set tf [open ${variant}_${rate}_output.tr w]
+set tf [open ${v1}_${v2}_${rate}_output.tr w]
 $ns trace-all $tf
 
 # Define a 'finish' procedure
@@ -25,7 +26,6 @@ set n4 [$ns node]
 set n5 [$ns node]
 set n6 [$ns node]
 
-
 # Create links between the nodes
 $ns duplex-link $n1 $n2 10Mb 10ms DropTail
 $ns duplex-link $n5 $n2 10Mb 10ms DropTail
@@ -34,62 +34,77 @@ $ns duplex-link $n3 $n4 10Mb 10ms DropTail
 $ns duplex-link $n3 $n6 10Mb 10ms DropTail
 
 
-# Setup a TCP connection
-if {$variant eq "Tahoe"} {
-	set tcp [new Agent/TCP]
-} elseif {$variant eq "Reno"} {
-	set tcp [new Agent/TCP/Reno]
-} elseif {$variant eq "NewReno"} {
-	set tcp [new Agent/TCP/Newreno]
-} elseif {$variant eq "Vegas"} {
-	set tcp [new Agent/TCP/Vegas]
+
+# Set up first TCP connection
+if {$v1 eq "Reno"} {
+	set tcp1 [new Agent/TCP/Reno]
+} elseif {$v1 eq "NewReno"} {
+	set tcp1 [new Agent/TCP/Newreno]
+} elseif {$v1 eq "Vegas"} {
+	set tcp1 [new Agent/TCP/Vegas]
 }
-$tcp set class_ 2
-$ns attach-agent $n1 $tcp
-set sink [new Agent/TCPSink]
-$ns attach-agent $n4 $sink
-$ns connect $tcp $sink
+$tcp1 set class_ 2
+$ns attach-agent $n1 $tcp1
+set sink1 [new Agent/TCPSink]
+$ns attach-agent $n4 $sink1
+$ns connect $tcp1 $sink1
 $tcp set fid_ 1
 
 
-# Setup FTP over TCP
-set ftp [new Application/FTP]
-$ftp attach-agent $tcp
-$ftp set type_ FTP
+# Set up second TCP connection
+if {$v2 eq "Reno"} {
+	set tcp2 [new Agent/TCP/Reno]
+} elseif {$v2 eq "Vegas"} {
+	set tcp2 [new Agent/TCP/Vegas]
+}
+$tcp2 set class_ 3
+$ns attach-agent $n5 $tcp2
+set sink2 [new Agent/TCPSink]
+$ns attach-agent $n6 $sink2
+$ns connect $tcp2 $sink2
+$tcp1 set fid_ 3
 
-# Add a CBR source at N2
+# Setup first FTP over TCP connection
+set ftp1 [new Application/FTP]
+$ftp1 attach-agent $tcp1
+
+# Setup second FTP over TCP connection
+set ftp2 [new Application/FTP]
+$ftp2 attach-agent $tcp2
+
 # Setup a UDP connection
 set udp [new Agent/UDP]
 $ns attach-agent $n2 $udp
 set null [new Agent/Null]
 $ns attach-agent $n3 $null
 $ns connect $udp $null
-$udp set fid_ 2
 
 # Setup a CBR over UDP connection
 set cbr [new Application/Traffic/CBR]
 $cbr attach-agent $udp
 $cbr set type_ CBR
 $cbr set packet_size_ 1000
+# Change rate until TCP can reach its bottleneck
 $cbr set rate_ ${rate}mb
 $cbr set random_ false
-
 
 # Schedule events for the CBR and FTP agents
 $ns at 0.1 "$cbr start"
 $ns at 1.0 "$ftp start"
+$ns at 1.0 "$ftp1 start"
 $ns at 8.5 "$ftp stop"
+$ns at 8.5 "$ftp1 stop"
 $ns at 9.5 "$cbr stop"
 
 # Call the finish procedure
 $ns at 10.0 "finish"
 
-# Print CBR packet size and interval
+#Print CBR packet size and interval
 puts "CBR packet size = [$cbr set packet_size_]"
 puts "CBR interval = [$cbr set interval_]"
 
-# Run the simulation
+#Run the simulation
 $ns run
 
-close $tf
 
+close $tf
